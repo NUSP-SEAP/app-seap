a347d736730bd2c9dd000b24ae071cf582dfc92c
Douglas <douglas.antunes.sen@outlook.com>
2025-11-20 23:50:10 +0000

    Bloco async function initializePage() de home.js comentado

--- app/js/app/home.js
+++ app/js/app/home.js
@@ -17,14 +17,14 @@ function loadScript(src) {
 }
 
 // --- Lógica de inicialização ---
-async function initializePage() {
-    // Espera os componentes HTML terminarem de carregar
-    await Promise.all([
-        loadComponent("header", "/components/header.html"),
-        loadComponent("footer", "/components/footer.html")
-    ]);
+// async function initializePage() {
+//     // Espera os componentes HTML terminarem de carregar
+//     await Promise.all([
+//         loadComponent("header", "/components/header.html"),
+//         loadComponent("footer", "/components/footer.html")
+//     ]);
 
-    // SÓ DEPOIS de carregar o HTML, carrega o script
-}
+//     // SÓ DEPOIS de carregar o HTML, carrega o script
+// }
 
-initializePage(); // Executa a inicialização
+// initializePage(); // Executa a inicialização
